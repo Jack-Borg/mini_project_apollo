30954b4da94b83d23490838ac50f4dd0d27de824
jack-borg <jacob.fam.borg@gmail.com>
2019-05-29 18:58:44 +0200

    build

---
+++ build/precache-manifest.245e19e86fd51895c50642a92cd8c493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ed370c123ca65089f6d915bd061a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcfcc54dddbefafdaa9b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "451df10da8349b538716",
+    "url": "/static/js/2.6a7f5bdd.chunk.js"
+  },
+  {
+    "revision": "bcfcc54dddbefafdaa9b",
+    "url": "/static/js/main.d39847c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
